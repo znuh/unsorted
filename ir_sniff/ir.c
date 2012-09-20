b45ed3791e0b2008e5a16065833bd65dd5bfa9fc
Znuh <Zn000h@gmail.com>
2012-09-20 18:46:19 +0200

    indent

--- ir_sniff/ir.c
+++ ir_sniff/ir.c
@@ -4,25 +4,26 @@
 #include <util/delay.h>
 #include <stdint.h>
 
-static uint8_t bitcnt=0;
+static uint8_t bitcnt = 0;
 static uint8_t byte;
 
 static uint16_t etu;
 
 static volatile uint8_t rbuf[256];
-static volatile uint8_t rb_fill=0;
-static uint8_t rb_put=0;
+static volatile uint8_t rb_fill = 0;
+static uint8_t rb_put = 0;
 
 // TODOs: F_CPU fast enough? ringbuffer large enough? testing!
 
-ISR(TIMER1_COMPA_vect) {
+ISR(TIMER1_COMPA_vect)
+{
 	TCNT1 = 0;
-	byte>>=1;
-	byte|=0x80;
-	if(bitcnt == 8) {
-		TIMSK &= ~(1<<OCIE1A);
+	byte >>= 1;
+	byte |= 0x80;
+	if (bitcnt == 8) {
+		TIMSK &= ~(1 << OCIE1A);
 		//UDR = byte;
-		rbuf[rb_put++]=byte;
+		rbuf[rb_put++] = byte;
 		rb_fill++;
 		bitcnt = 0;
 		return;
@@ -31,61 +32,62 @@ ISR(TIMER1_COMPA_vect) {
 	bitcnt++;
 }
 
-ISR(TIMER1_CAPT_vect) {
+ISR(TIMER1_CAPT_vect)
+{
 	uint16_t val;
-	
+
 	TCNT1 = 0;
 	val = ICR1;
-	
-	if(TCCR1B & (1<<ICES1)) {
+
+	if (TCCR1B & (1 << ICES1)) {
 		// rising edge - add a zero
-		byte>>=1;
-		if(bitcnt == 8) {
+		byte >>= 1;
+		if (bitcnt == 8) {
 			//UDR = byte;
-			rbuf[rb_put++]=byte;
+			rbuf[rb_put++] = byte;
 			rb_fill++;
 			bitcnt = 0;
 			return;
 		}
 		bitcnt++;
-	}
-	else {
+	} else {
 		// falling edge
 		// setup COMPA - val is 3/16 of a bit
-		etu = (val*16)/3;
-		OCR1A = etu + etu/2;
-		TIFR = (1<<OCF1A);
-		TIMSK = (1<<OCIE1A);
+		etu = (val * 16) / 3;
+		OCR1A = etu + etu / 2;
+		TIFR = (1 << OCF1A);
+		TIMSK = (1 << OCIE1A);
 	}
-	TCCR1B ^= (1<<ICES1);
+	TCCR1B ^= (1 << ICES1);
 }
 
-int main(void) {
-	uint8_t rb_get=0;
-	
-	UBRRH=0;
-	UBRRL = 7; // 115.2 kBaud
-	
-	UCSRB = (1<<TXEN);
-	UCSRC = (1<<URSEL)|(3<<UCSZ0); // 8 bit, 1 stopbit
-	
+int main(void)
+{
+	uint8_t rb_get = 0;
+
+	UBRRH = 0;
+	UBRRL = 7;		// 115.2 kBaud
+
+	UCSRB = (1 << TXEN);
+	UCSRC = (1 << URSEL) | (3 << UCSZ0);	// 8 bit, 1 stopbit
+
 	//UDR = 0; // test
-	
+
 	TCCR1A = 0;
-	TCCR1B = (1<<ICNC1)|(1<<ICES1)|(1<<CS10);
-	TIMSK = (1<<TICIE1);
-	
+	TCCR1B = (1 << ICNC1) | (1 << ICES1) | (1 << CS10);
+	TIMSK = (1 << TICIE1);
+
 	sei();
-	
-	while(1) {
+
+	while (1) {
 		loop_until_bit_is_set(UCSRA, UDRE);
 		cli();
-		if(rb_fill) {
+		if (rb_fill) {
 			rb_fill--;
 			UDR = rbuf[rb_get++];
 		}
 		sei();
 	}
-	
+
 	return 0;
 }
