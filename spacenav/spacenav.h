4117047653f4ed5fdfd3332c757115d9f2d09a94
hunz <Zn000h@gmail.com>
2013-03-11 01:32:12 +0100

    fix reset-to-zero

---
+++ spacenav/spacenav.h
@@ -0,0 +1,28 @@
+#ifndef SPACENAV_H
+#define SPACENAV_H
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <linux/types.h>
+#include <pthread.h>
+
+typedef struct sn_axes_s {
+	__s32 x, y, z;
+	__s32 rx, ry, rz;
+	__s32 btn_l, btn_r;
+} sn_axes_t;
+
+typedef struct spavenav_s {
+	int fd;
+	int quit;
+	pthread_mutex_t mtx;
+	pthread_t sn_thread;
+	sn_axes_t axes;
+	struct timeval tm[6];
+} spacenav_t;
+
+spacenav_t *spacenav_create(char *dev);
+void spacenav_destroy(spacenav_t *sn);
+void spacenav_get(spacenav_t *sn, sn_axes_t *axes);
+
+#endif
