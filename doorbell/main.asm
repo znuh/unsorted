f191f524b8053ae003bb480396230e7a95972e11
Hunz <Zn000h@gmail.com>
2015-12-20 16:46:51 +0100

    fix formatting

--- doorbell/main.asm
+++ doorbell/main.asm
@@ -18,73 +18,73 @@
 ; Y: 28/29
 ; Z: 30/31
 
-    ; RESET
-    rjmp RESET
-    ; INT0
-    reti
-    ; PCINT0
-    rjmp PCINT
-    ; TIM0_OVF
-    rjmp T0_OVF
-    ; EE_RDY
-    reti
-    ; ANA_COMP
-    reti
-    ; TIM0_COMPA
-    reti
-    ; TIM0_COMPB
-    reti
-    ; WATCHDG
-    reti
-    ; ADC
-    reti
+	; RESET
+	rjmp RESET
+	; INT0
+	reti
+	; PCINT0
+	rjmp PCINT
+	; TIM0_OVF
+	rjmp T0_OVF
+	; EE_RDY
+	reti
+	; ANA_COMP
+	reti
+	; TIM0_COMPA
+	reti
+	; TIM0_COMPB
+	reti
+	; WATCHDG
+	reti
+	; ADC
+	reti
 
 T0_OVF:
 	; >= 1 minute since last ring?
 	cpi ovfcnt, 3
 	brsh TIMEOUT
-	
+
 	inc ovfcnt
 	reti
-	
+
 TIMEOUT:
 	clr ringcnt
 	reti
-    
+
 PCINT:
 	; ringing signal present?
 	sbis PINB, PB1
 	rjmp RING_END
-	
+
 	; ringing starts
-	
+
 	; reset timeout
 	clr ovfcnt
-	
+
 	; ring after 2nd time
 	cpi ringcnt, 2
 	brsh RING
-	
+
 	inc ringcnt
 	reti
-	
+
 RING:
 	sbi PORTB, PB0
 	reti
-	
+
 RING_END:
 	; disable NMOS/relais
 	; saves some power (100k pulldown on NMOS gate)
 	cbi PORTB, PB0
-    reti
+	reti
 
-; PB1: INT1 - power detect (AIN1)
-; PB0: NMOS/relais enable (AIN0)
-    
+; PB1: power detect
+; PB0: NMOS/relais enable
+
 RESET:
 	; enable output (low) for PB0 - disable relais
 	sbi DDRB, PB0
-	
+
 	; heavy power-saving ahead
 
 	; enable pullups for all IOs except PB1/PB0
@@ -94,40 +94,40 @@ RESET:
 	; input disable for all IOs except for PB1 (power detect)
 	ldi temp, $3D
 	out DIDR0, temp
-	
+
 	; disable analog comparator
 	sbi ACSR, ACD
-	
+
 	; setup stack pointer
-    ldi temp, low(RAMEND)
-    out SPL, temp
-    
-    ; configure timer0
-    ; 16kHz/1024 = 15.6Hz
-    ; -> overflow after ~16.4secs
+	ldi temp, low(RAMEND)
+	out SPL, temp
+
+	; configure timer0
+	; 16kHz/1024 = 15.6Hz
+	; -> overflow after ~16.4secs
 	ldi temp, (1<<CS02)|(1<<CS00)
 	out TCCR0B, temp
 
 	; prepare for idle mode
 	ldi temp, (1<<SE)
 	out MCUCR, temp
-	
+
 	; enable T0 OVF int
 	ldi temp, (1<<TOIE0)
 	out TIMSK0, temp
-	
+
 	; enable PCINT1
 	sbi PCMSK, PCINT1
-	
+
 	; enable pin change int
 	ldi temp, (1<<PCIE)
 	out GIMSK, temp
-	
+
 	ldi ringcnt, 1
 	clr ovfcnt
-	
+
 	sei
-	
-loop:
+
+LOOP:
 	sleep
-	rjmp loop
+	rjmp LOOP
